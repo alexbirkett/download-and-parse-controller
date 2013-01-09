c40901ac80e9954abff280e43ec1298075093f7f
Alex Birkett <alex@birkett.no>
2013-01-09 09:07:32 +0100

    Disconnect urlConnection after use Use BufferedInputStream

--- src/com/birkett/downloadandparsecontroller/DownloadAndParseTask.java
+++ src/com/birkett/downloadandparsecontroller/DownloadAndParseTask.java
@@ -1,9 +1,10 @@
 package com.birkett.downloadandparsecontroller;
 
+import java.io.BufferedInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.HttpURLConnection;
 import java.net.URL;
-import java.net.URLConnection;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import android.os.AsyncTask;
@@ -25,11 +26,16 @@ public class DownloadAndParseTask extends AsyncTask<String, Integer, Object> {
 	protected Object doInBackground(String... sUrl) {
 
 		Object object = null;
+		HttpURLConnection urlConnection = null;
 		try {
-			InputStream inputStream = download(sUrl[0]);
+			URL url = new URL(sUrl[0]);
+			urlConnection = (HttpURLConnection) url.openConnection();
+			InputStream inputStream = new BufferedInputStream(urlConnection.getInputStream());
 			object = mMapper.readValue(inputStream, Object.class);
 		} catch (IOException e) {
 			mException = e;
+		} finally {
+			urlConnection.disconnect();
 		}
 
 		return object;
@@ -50,13 +56,4 @@ public class DownloadAndParseTask extends AsyncTask<String, Integer, Object> {
 	protected void onPreExecute() {
 		mObserver.onDownloadAndParseStarted();
     }
-	 
-	private InputStream download(String urlString) throws IOException {
-		URL url = new URL(urlString);
-		URLConnection connection = url.openConnection();
-		connection.connect();
-		InputStream input = url.openStream();
-		return input;
-	}
-	
 }
